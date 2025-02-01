847917bfb0e98604551cebd5d70e9c8a7eab376f
shreya616sharma <157040145+shreya616sharma@users.noreply.github.com>
2025-02-01 18:51:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,6 +4,7 @@
 int main()
 {
     int a,b;
+    float d;
     char c;
     scanf("%d %d %c",&a,&b,&c);
     
@@ -17,7 +18,13 @@ int main()
         printf("%d",(a*b));
     }
     else{
-        printf("%d",(a/b));
+        if( b == 0){
+            printf("error");
+        }
+        else{
+            d = (float)b;
+            printf("%d",(a/b.0));
+        }
     }
     return 0;
 }
