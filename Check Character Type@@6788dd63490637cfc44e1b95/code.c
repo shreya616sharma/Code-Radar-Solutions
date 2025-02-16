8b68cdf877895d8503089b19673d2aabae2ff5c3
shreya616sharma <157040145+shreya616sharma@users.noreply.github.com>
2025-02-16 15:33:34 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -20,6 +20,21 @@ int main()
         case 'u':
         printf("Vowel");
         break;
+        case 'A':  
+            printf("Vowel");
+            break;
+        case 'E':
+            printf("Vowel");
+            break;
+        case 'I':
+            printf("Vowel");
+            break;
+        case 'O':
+            printf("Vowel");
+            break;
+        case 'U':
+            printf("Vowel");
+            break;
         default:
         printf("Consonant");
         break;
